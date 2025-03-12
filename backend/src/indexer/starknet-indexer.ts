e6ec7ab2744b34aae3312b270e748795a84dc774
lukalazarevic123 <lazarevicluka99@gmail.com>
2025-03-12 18:03:50 +0100

    WIP: write data into the database

--- backend/src/indexer/starknet-indexer.ts
+++ backend/src/indexer/starknet-indexer.ts
@@ -1,17 +1,17 @@
-// import { Pool } from "pg";
+import { Pool } from "pg";
 import { ContractListener } from "../listener/contract-listener"
 import { IndexerOptions, ListenerData } from "../types";
 
 
 export class Indexer {
-    // private pool: Pool;
+    private pool: Pool;
     private announcerListener: ContractListener;
     private metaListener: ContractListener;
-    // private options: IndexerOptions
+    private options: IndexerOptions
 
     constructor(options: IndexerOptions) {
-        // this.options = options;
-        // this.pool = new Pool(this.options.dbConfig);
+        this.options = options;
+        this.pool = new Pool(this.options.dbConfig);
 
         this.announcerListener = new ContractListener({
             rpcUrl: options.rpcUrl,
@@ -53,7 +53,32 @@ export class Indexer {
     }
 
     private async handleAnnouncerEvent(data: ListenerData) {
-        console.log("ANNOUNCER:",data)
+        console.log("ANNOUNCER:", data)
+        const { raw, tx, decoded } = data;
+
+       const ephemeralPublicKey = decoded[0];
+       const viewTag = decoded[1];
+       const stealthAccountAddress = decoded[3];
+
+       const amount = 0;
+       const blockNumber = raw.block_number;
+       const hash = raw.transaction_hash;
+       const sender = tx.result.sender_address;
+
+        const query = `
+      INSERT INTO announcements
+        (sender, stealth_address, amount, ephemeral_public_key, view_tag, created_at, block_number, hash)
+      VALUES
+        ($1, $2, $3, $4, $5, NOW(), $6, $7)
+    `;
+        const values = [sender, stealthAccountAddress, amount, ephemeralPublicKey, viewTag, blockNumber, hash];
+
+        try {
+            await this.pool.query(query, values);
+            console.log(`Inserted announcement event ${hash}`);
+        } catch (err) {
+            console.error("Error inserting announcement event:", err);
+        }
     }
 
     private async handleMetaEvent(data: ListenerData) {
