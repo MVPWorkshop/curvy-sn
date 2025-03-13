f2e2b8b50e227ec9e1abe97004cd4928322bed73
bojinovic <bojinovicc@hotmail.com>
2025-03-13 16:41:55 +0100

    indexer: decoding events fix

---
+++ backend/src/listener/parser.ts
@@ -0,0 +1,102 @@
+import { validateAndParseAddress } from "starknet";
+import { TOKENS_OF_INTEREST } from "./constants";
+
+export interface ParsedCall {
+    contractAddress: string;
+    entrypoint: string;
+    calldata: string[];
+}
+
+export interface TokenTransfer {
+    recipient: string;
+    amount: string;
+    name: string;
+}
+
+export const extractAllCalls = (data: string[]): Array<ParsedCall> | null => {
+    // Helper article:
+    //  https://9oelm.github.io/2024-01-26-decoding-calldata-of-transactions-on-starknet/
+    // New format:
+    //  - number of calls
+    //  - array of individual calls [] {
+    //     - contract address for which the call was made
+    //     - entrypoint of the called method
+    //     - length of the calldata for this call
+    //  }
+
+    try {
+        const nCalls = parseInt(data[0], 16);
+
+        const calls: Array<ParsedCall> = [];
+
+        let offset = 1;
+        for (let i = 0; i < nCalls; ++i) {
+            const contractAddress = validateAndParseAddress(data[offset]);
+            const entrypoint = data[offset + 1];
+            const calldataLength = parseInt(data[offset + 2], 16);
+            const calldata = data.slice(
+                offset + 3,
+                offset + 2 + calldataLength + 1
+            );
+            calls.push({ contractAddress, entrypoint, calldata });
+            offset = offset + 3 + calldataLength;
+        }
+
+        return calls;
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+};
+
+export const extractByteArray = (
+    data: string[]
+): { res: string; offset: number } => {
+    // ByteArray is encoding ref:
+    //  https://docs.starknet.io/architecture-and-concepts/smart-contracts/serialization-of-cairo-types/#serialization_of_byte_arrays
+    // Data Structure:
+    //  - number of full 31 byte words in the byte array
+    //  - ...31 byte words...
+    //  - pending word (remaining bytes that do not add up to 31 bytes in total)
+    //  - length of the pending word in bytes
+
+    let res: string = "";
+
+    const countFull31ByteWords = parseInt(data[0], 16);
+    let offset = 1;
+    // note: implicit append of pending word also
+    for (; offset < 2 + countFull31ByteWords; ++offset) {
+        res += data[offset].replace("0x", "");
+    }
+
+    offset += 1; // note: skip over the length of the pending word field
+    return { res, offset };
+};
+
+export const parseERC20Transfers = (
+    calls: Array<ParsedCall>
+): Array<TokenTransfer> => {
+    const transfers: Array<TokenTransfer> = [];
+
+    try {
+        for (const call of calls) {
+            const TOI = TOKENS_OF_INTEREST as any;
+            const tokenAddress = validateAndParseAddress(call.contractAddress);
+            const tokenInfo = TOI[tokenAddress];
+            if (
+                tokenInfo === undefined ||
+                tokenInfo.transferMethodSignature !== call.entrypoint
+            )
+                continue;
+
+            const recipient = validateAndParseAddress(call.calldata[0]);
+            const amount = `${
+                parseInt(call.calldata[1], 16) / 10 ** tokenInfo.decimals
+            }`;
+            transfers.push({ recipient, amount, name: tokenInfo.name });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    return transfers;
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,14 @@
-import express, { Router } from 'express';
+import express, { Router } from "express";
 import * as http from "http";
 import { json, urlencoded } from "body-parser";
-import { AppRouting } from './router/app-routing';
-import { Pool } from 'pg';
-import { ContractListener } from './listener/contract-listener';
+import { AppRouting } from "./router/app-routing";
+import { Pool } from "pg";
+import { ContractListener } from "./listener/contract-listener";
 import announcerArtifact from "./artifacts/curvy_announcer_CurvyAnnouncerV0.contract_class.json";
 import metaRegistryArtifact from "./artifacts/curvy_meta_registry_CurvyMetaRegistryV0.contract_class.json";
 
-
 const path = require("path");
-require('dotenv').config();
+require("dotenv").config();
 
 export class Server {
     public app: express.Express;
@@ -41,14 +40,14 @@ export class Server {
                 "core::byte_array::ByteArray",
                 "core::byte_array::ByteArray",
                 "core::starknet::contract_address::ContractAddress",
-            ]
-        })
+            ],
+        });
 
         announcerListener.on("event", (data) => {
-            console.log("Announcer Event:", data)
-        })
+            console.log("Announcer Event:", data);
+        });
 
-        announcerListener.start()
+        announcerListener.start();
 
         const metaListener = new ContractListener({
             rpcUrl: process.env.RPC_URL!,
@@ -57,13 +56,13 @@ export class Server {
             chunkSize: 10,
             abi: metaRegistryArtifact.abi,
             decodeParameters: ["core::felt252", "core::byte_array::ByteArray"],
-        })
+        });
 
         metaListener.on("event", (data) => {
-            console.log("Meta Registry Event:", data)
-        })
+            console.log("Meta Registry Event:", data);
+        });
 
-        metaListener.start()
+        metaListener.start();
     }
 
     private configureMiddleware() {
@@ -75,7 +74,9 @@ export class Server {
         const basePath = "/";
         this.app.use(basePath, this.router);
         if (process.env.NODE_ENV == "production") {
-            this.app.use(express.static(path.join(__dirname, '/../client/build')));
+            this.app.use(
+                express.static(path.join(__dirname, "/../client/build"))
+            );
         }
 
         new AppRouting(this.router);
@@ -88,20 +89,20 @@ export class Server {
             port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
             user: process.env.PGUSER,
             password: process.env.PGPASSWORD,
-            database: process.env.PGDATABASE
+            database: process.env.PGDATABASE,
         });
 
         // Test the PostgreSQL connection
         this.pool.connect((err, client, release) => {
             if (err) {
-                console.error('Error acquiring client', err.stack);
+                console.error("Error acquiring client", err.stack);
             } else {
-                client?.query('SELECT NOW()', (err, result) => {
+                client?.query("SELECT NOW()", (err, result) => {
                     release();
                     if (err) {
-                        console.error('Error executing query', err.stack);
+                        console.error("Error executing query", err.stack);
                     } else {
-                        console.log('Postgres connected:', result.rows[0]);
+                        console.log("Postgres connected:", result.rows[0]);
                     }
                 });
             }
@@ -112,13 +113,12 @@ export class Server {
         const port = process.env.PORT || 4000;
         const server = http.createServer(this.app);
 
-
         //Docker sends SIGTERM signal when using docker stop
         //If this isn't present the container will take much longer to be terminated
         process.on("SIGTERM", function (code_signal_error) {
-            console.log(code_signal_error)
-            process.exit(0) //or whatever you want
-        })
+            console.log(code_signal_error);
+            process.exit(0); //or whatever you want
+        });
 
         server.listen(port, () => {
             console.log(`Express server running on port ${port}.`);
