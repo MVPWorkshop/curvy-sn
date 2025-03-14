6099b0e58794204993e02afcf3a8f3227cb44756
bojinovic <bojinovicc@hotmail.com>
2025-03-14 16:20:40 +0100

    sc: added interaction example

--- sc/script/deploy_protocol.js
+++ sc/script/deploy_protocol.js
@@ -1,24 +1,9 @@
-import {
-  ec,
-  hash,
-  num,
-  json,
-  Contract,
-  EthSigner,
-  CallData,
-  Account,
-  RpcProvider,
-  stark,
-  cairo,
-  eth,
-} from "starknet";
-
 import "dotenv/config";
 
 import {
-  initDeployerAccount,
-  declareAllContracts,
-  deployProtocol,
+    initDeployerAccount,
+    declareAllContracts,
+    deployProtocol,
 } from "./_utils.js";
 
 const { account, provider } = initDeployerAccount();
@@ -27,4 +12,6 @@ const declaredContracts = await declareAllContracts(account);
 
 console.log({ declaredContracts });
 
-await deployProtocol(account, declaredContracts);
+const deployed = await deployProtocol(account, declaredContracts);
+
+console.log({ deployed });
