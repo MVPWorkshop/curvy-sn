ef7eae973c33c1b1b01f9b1fd7e04c70d7242028
bojinovic <bojinovicc@hotmail.com>
2025-03-18 16:15:04 +0100

    bd: stress test param. cfg

--- backend/test/stress-test/config.js
+++ backend/test/stress-test/config.js
@@ -11,10 +11,10 @@ export let miniConfigOptions = {
 // Configure load stages for stress and spike testing.
 export let defaultConfigOptions = {
     stages: [
-        { duration: "30s", target: 20 }, // Ramp-up to 15 VUs in 30 seconds.
-        { duration: "1m", target: 20 }, // Steady load for 1 minute.
-        // { duration: "30s", target: 30 }, // Spike up to 30 VUs for 30 seconds.
-        // { duration: "1m", target: 30 }, // Hold the spike for 1 minute.
-        // { duration: "30s", target: 0 }, // Ramp-down to 0 VUs.
+        { duration: "20s", target: 15 },
+        { duration: "1m", target: 15 },
+        { duration: "30s", target: 50 },
+        { duration: "20s", target: 50 },
+        { duration: "30s", target: 0 },
     ],
 };
